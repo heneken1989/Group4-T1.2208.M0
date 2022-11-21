3c6b80bfd22d3b4c6776e106046218b73e88eeb0
heneken1989 <116566386+heneken1989@users.noreply.github.com>
2022-11-21 18:43:41 +0900

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -8,23 +8,23 @@ app.config([
   "$routeProvider",
   function ($routeProvider) {
     $routeProvider
-      .when("", { templateUrl: "./Page/home.html" })
-      .when("/problem", { templateUrl: "./Page/problem.html" })
-      .when("/meditation", { templateUrl: "./Page/meditation.html" })
+      .when("", { templateUrl: "Page/home.html" })
+      .when("/problem", { templateUrl: "Page/problem.html" })
+      .when("/meditation", { templateUrl: "Page/meditation.html" })
       .when("/", {
-        templateUrl: "./Page/customAerobic.html",
+        templateUrl: "Page/customAerobic.html",
         controller: "ctrlAerobic",
       })
       .when("/login", {
-        templateUrl: "./Page/login.html",
+        templateUrl: "Page/login.html",
         controller: "ctrlLogin",
       })
       .when("/displayDataLocal", {
-        templateUrl: "./Page/displayDataLocal.html",
+        templateUrl: "Page/displayDataLocal.html",
         controller: "ctrlLogin",
       })
       .when("/", {
-        templateUrl: "./Page/customAerobic.html",
+        templateUrl: "Page/customAerobic.html",
         controller: "customCtrl",
       });
   },
