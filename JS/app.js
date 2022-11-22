6f4a515530b204e250e5e97d9389e8ac936046d0
PT6402 <111172333+PT6402@users.noreply.github.com>
2022-11-22 20:56:02 +0700

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,27 +1,30 @@
 var app = angular.module("myApp", [
-  "ngRoute","ngMessages","dndLists"
+  "ngRoute",
+  "ngSanitize",
+  "dndLists",
+  "ngMessages",
 ]);
 app.config([
   "$routeProvider",
   function ($routeProvider) {
     $routeProvider
-      .when("", { templateUrl: "Page/home.html" })
-      .when("/problem", { templateUrl: "Page/problem.html" })
-      .when("/meditation", { templateUrl: "Page/meditation.html" })
+      .when("", { templateUrl: "./Page/home.html" })
+      .when("/problem", { templateUrl: "./Page/problem.html" })
+      .when("/meditation", { templateUrl: "./Page/meditation.html" })
       .when("/", {
-        templateUrl: "Page/customAerobic.html",
+        templateUrl: "./Page/customAerobic.html",
         controller: "ctrlAerobic",
       })
       .when("/login", {
-        templateUrl: "Page/login.html",
+        templateUrl: "./Page/login.html",
         controller: "ctrlLogin",
       })
       .when("/displayDataLocal", {
-        templateUrl: "Page/displayDataLocal.html",
+        templateUrl: "./Page/displayDataLocal.html",
         controller: "ctrlLogin",
       })
       .when("/", {
-        templateUrl: "Page/customAerobic.html",
+        templateUrl: "./Page/customAerobic.html",
         controller: "customCtrl",
       });
   },
@@ -53,7 +56,7 @@ app.controller("HeaderFooter", function ($scope) {
   // =====================================
 });
 app.run(function ($rootScope, $http) {
-  $http.get("JSON/product.json").then(function (rsp) {
+  $http.get("../JSON/product.json").then(function (rsp) {
     $rootScope.products = rsp.data.product;
     $rootScope.listPractice = [];
     $rootScope.saveValue = [];
@@ -892,287 +895,6 @@ app.directive("compareTo", function () {
   };
 });
 
-let allMusic = [
-  {
-    name: "Harley Bird - Home",
-    artist: "Jordan Schor",
-    img: "music-1",
-    src: "music-1",
-  },
-  {
-    name: "Ikson Anywhere – Ikson",
-    artist: "Audio Library",
-    img: "music-2",
-    src: "music-2",
-  },
-  {
-    name: "Beauz & Jvna - Crazy",
-    artist: "Beauz & Jvna",
-    img: "music-3",
-    src: "music-3",
-  },
-  {
-    name: "Hardwind - Want Me",
-    artist: "Mike Archangelo",
-    img: "music-4",
-    src: "music-4",
-  },
-  {
-    name: "Jim - Sun Goes Down",
-    artist: "Jim Yosef x Roy",
-    img: "music-5",
-    src: "music-5",
-  },
-  {
-    name: "Lost Sky - Vision NCS",
-    artist: "NCS Release",
-    img: "music-6",
-    src: "music-6",
-  },
-  // like this paste it and remember to give comma after ending of this bracket }
-  // {
-  //   name: "Here is the music name",
-  //   artist: "Here is the artist name",
-  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
-  // }
-];
-const wrapper = document.querySelector(".wrapper"),
-  musicImg = wrapper.querySelector(".img-area img"),
-  musicName = wrapper.querySelector(".song-details .name"),
-  musicArtist = wrapper.querySelector(".song-details .artist"),
-  playPauseBtn = wrapper.querySelector(".play-pause"),
-  prevBtn = wrapper.querySelector("#prev"),
-  nextBtn = wrapper.querySelector("#next"),
-  mainAudio = wrapper.querySelector("#main-audio"),
-  progressArea = wrapper.querySelector(".progress-area"),
-  progressBar = progressArea.querySelector(".progress-bar"),
-  musicList = wrapper.querySelector(".music-list"),
-  moreMusicBtn = wrapper.querySelector("#more-music"),
-  closemoreMusic = musicList.querySelector("#close");
-
-let musicIndex = Math.floor(Math.random() * allMusic.length + 1);
-isMusicPaused = true;
-window.addEventListener("load", () => {
-  loadMusic(musicIndex);
-  playingSong();
-});
-
-function loadMusic(indexNumb) {
-  musicName.innerText = allMusic[indexNumb - 1].name;
-  musicArtist.innerText = allMusic[indexNumb - 1].artist;
-  musicImg.src = `./Page/IMGS/${allMusic[indexNumb - 1].src}.jpg`;
-  mainAudio.src = `./Page/audio/${allMusic[indexNumb - 1].src}.mp3`;
-}
-
-//play music function
-function playMusic() {
-  wrapper.classList.add("paused");
-  playPauseBtn.querySelector("i").innerText = "pause";
-  mainAudio.play();
-}
-
-//pause music function
-function pauseMusic() {
-  wrapper.classList.remove("paused");
-  playPauseBtn.querySelector("i").innerText = "play_arrow";
-  mainAudio.pause();
-}
-
-//prev music function
-function prevMusic() {
-  musicIndex--; //decrement of musicIndex by 1
-  //if musicIndex is less than 1 then musicIndex will be the array length so the last music play
-  musicIndex < 1 ? (musicIndex = allMusic.length) : (musicIndex = musicIndex);
-  loadMusic(musicIndex);
-  playMusic();
-  playingSong();
-}
-
-//next music function
-function nextMusic() {
-  musicIndex++; //increment of musicIndex by 1
-  //if musicIndex is greater than array length then musicIndex will be 1 so the first music play
-  musicIndex > allMusic.length ? (musicIndex = 1) : (musicIndex = musicIndex);
-  loadMusic(musicIndex);
-  playMusic();
-  playingSong();
-}
-// play or pause button event
-playPauseBtn.addEventListener("click", () => {
-  const isMusicPlay = wrapper.classList.contains("paused");
-  //if isPlayMusic is true then call pauseMusic else call playMusic
-  isMusicPlay ? pauseMusic() : playMusic();
-  playingSong();
-});
-
-//prev music button event
-prevBtn.addEventListener("click", () => {
-  prevMusic();
-});
-
-//next music button event
-nextBtn.addEventListener("click", () => {
-  nextMusic();
-});
-// update progress bar width according to music current time
-mainAudio.addEventListener("timeupdate", (e) => {
-  const currentTime = e.target.currentTime; //getting playing song currentTime
-  const duration = e.target.duration; //getting playing song total duration
-  let progressWidth = (currentTime / duration) * 100;
-  progressBar.style.width = `${progressWidth}%`;
-
-  let musicCurrentTime = wrapper.querySelector(".current-time"),
-    musicDuartion = wrapper.querySelector(".max-duration");
-  mainAudio.addEventListener("loadeddata", () => {
-    // update song total duration
-    let mainAdDuration = mainAudio.duration;
-    let totalMin = Math.floor(mainAdDuration / 60);
-    let totalSec = Math.floor(mainAdDuration % 60);
-    if (totalSec < 10) {
-      //if sec is less than 10 then add 0 before it
-      totalSec = `0${totalSec}`;
-    }
-    musicDuartion.innerText = `${totalMin}:${totalSec}`;
-  });
-  // update playing song current time
-  let currentMin = Math.floor(currentTime / 60);
-  let currentSec = Math.floor(currentTime % 60);
-  if (currentSec < 10) {
-    //if sec is less than 10 then add 0 before it
-    currentSec = `0${currentSec}`;
-  }
-  musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
-});
-
-// update playing song currentTime on according to the progress bar width
-progressArea.addEventListener("click", (e) => {
-  let progressWidth = progressArea.clientWidth; //getting width of progress bar
-  let clickedOffsetX = e.offsetX; //getting offset x value
-  let songDuration = mainAudio.duration; //getting song total duration
-
-  mainAudio.currentTime = (clickedOffsetX / progressWidth) * songDuration;
-  playMusic(); //calling playMusic function
-  playingSong();
-});
-
-//change loop, shuffle, repeat icon onclick
-const repeatBtn = wrapper.querySelector("#repeat-plist");
-repeatBtn.addEventListener("click", () => {
-  let getText = repeatBtn.innerText; //getting this tag innerText
-  switch (getText) {
-    case "repeat":
-      repeatBtn.innerText = "repeat_one";
-      repeatBtn.setAttribute("title", "Song looped");
-      break;
-    case "repeat_one":
-      repeatBtn.innerText = "shuffle";
-      repeatBtn.setAttribute("title", "Playback shuffled");
-      break;
-    case "shuffle":
-      repeatBtn.innerText = "repeat";
-      repeatBtn.setAttribute("title", "Playlist looped");
-      break;
-  }
-});
-
-//code for what to do after song ended
-mainAudio.addEventListener("ended", () => {
-  // we'll do according to the icon means if user has set icon to
-  // loop song then we'll repeat the current song and will do accordingly
-  let getText = repeatBtn.innerText; //getting this tag innerText
-  switch (getText) {
-    case "repeat":
-      nextMusic(); //calling nextMusic function
-      break;
-    case "repeat_one":
-      mainAudio.currentTime = 0; //setting audio current time to 0
-      loadMusic(musicIndex); //calling loadMusic function with argument, in the argument there is a index of current song
-      playMusic(); //calling playMusic function
-      break;
-    case "shuffle":
-      let randIndex = Math.floor(Math.random() * allMusic.length + 1); //genereting random index/numb with max range of array length
-      do {
-        randIndex = Math.floor(Math.random() * allMusic.length + 1);
-      } while (musicIndex == randIndex); //this loop run until the next random number won't be the same of current musicIndex
-      musicIndex = randIndex; //passing randomIndex to musicIndex
-      loadMusic(musicIndex);
-      playMusic();
-      playingSong();
-      break;
-  }
-});
-
-//show music list onclick of music icon
-moreMusicBtn.addEventListener("click", () => {
-  musicList.classList.toggle("show");
-});
-closemoreMusic.addEventListener("click", () => {
-  moreMusicBtn.click();
-});
-
-const ulTag = wrapper.querySelector("ul");
-// let create li tags according to array length for list
-for (let i = 0; i < allMusic.length; i++) {
-  //let's pass the song name, artist from the array
-  let liTag = `<li li-index="${i + 1}">
-                <div class="row">
-                  <span>${allMusic[i].name}</span>
-                  <p>${allMusic[i].artist}</p>
-                </div>
-                <span id="${allMusic[i].src}" class="audio-duration">3:40</span>
-                <audio class="${allMusic[i].src}" src="./Page/audio/${
-    allMusic[i].src
-  }.mp3"></audio>
-              </li>`;
-  ulTag.insertAdjacentHTML("beforeend", liTag); //inserting the li inside ul tag
-
-  let liAudioDuartionTag = ulTag.querySelector(`#${allMusic[i].src}`);
-  let liAudioTag = ulTag.querySelector(`.${allMusic[i].src}`);
-  liAudioTag.addEventListener("loadeddata", () => {
-    let duration = liAudioTag.duration;
-    let totalMin = Math.floor(duration / 60);
-    let totalSec = Math.floor(duration % 60);
-    if (totalSec < 10) {
-      //if sec is less than 10 then add 0 before it
-      totalSec = `0${totalSec}`;
-    }
-    liAudioDuartionTag.innerText = `${totalMin}:${totalSec}`; //passing total duation of song
-    liAudioDuartionTag.setAttribute("t-duration", `${totalMin}:${totalSec}`); //adding t-duration attribute with total duration value
-  });
-}
-
-//play particular song from the list onclick of li tag
-function playingSong() {
-  const allLiTag = ulTag.querySelectorAll("li");
-
-  for (let j = 0; j < allLiTag.length; j++) {
-    let audioTag = allLiTag[j].querySelector(".audio-duration");
-
-    if (allLiTag[j].classList.contains("playing")) {
-      allLiTag[j].classList.remove("playing");
-      let adDuration = audioTag.getAttribute("t-duration");
-      audioTag.innerText = adDuration;
-    }
-
-    //if the li tag index is equal to the musicIndex then add playing class in it
-    if (allLiTag[j].getAttribute("li-index") == musicIndex) {
-      allLiTag[j].classList.add("playing");
-      audioTag.innerText = "Playing";
-    }
-
-    allLiTag[j].setAttribute("onclick", "clicked(this)");
-  }
-}
-
-//particular li clicked function
-function clicked(element) {
-  let getLiIndex = element.getAttribute("li-index");
-  musicIndex = getLiIndex; //updating current song index with clicked li index
-  loadMusic(musicIndex);
-  playMusic();
-  playingSong();
-}
 // ======================================================
 
 //Switch light/dark
@@ -1235,7 +957,7 @@ app.controller("customCtrl", function ($scope, $rootScope) {
                   <div class="wrapperPractice">
                     <div class="containerPractice">
                       <div class="topImgPracitceCard">
-                        <img src="${v.thumb}" alt="" />
+                        <img ng-src="${v.thumb}" alt="" />
                       </div>
                       <div class="bottomImgPracitceCard">
                         <div class="leftImgPracitceCard" >
@@ -1706,13 +1428,11 @@ app.controller("customCtrl", function ($scope, $rootScope) {
       }
       // alert(totalPractice);
 
-      $(".showTotalChoose").text(totalPractice);
+      $(".show_Total_Choose").text(totalPractice);
       if (totalPractice > 0) {
-        $(".labelTotal").css("display", "block");
-        $(".listCardPractice").css("left", "-400px");
+        $(".btnListChoose").css("display", "block");
       } else {
-        $(".listCardPractice").css("left", "-420px");
-        $(".labelTotal").css("display", "none");
+        $(".btnListChoose").css("display", "none");
         $("#tab-1").prop("checked", true);
       }
     };
@@ -1751,7 +1471,7 @@ app.controller("customCtrl", function ($scope, $rootScope) {
                   </div>
                 </div>
                 <div class="delete">
-                  <img class="delPracticeChoose"  data-index="${k}" src="IMG/close.png" alt="" />
+                  <img class="delPracticeChoose"  data-index="${k}" src="../IMG/close.png" alt="" />
                 </div>
               </div></li>`;
       });
@@ -1793,22 +1513,40 @@ app.controller("customCtrl", function ($scope, $rootScope) {
     // ============================================
     $(".buttonTimeList").on("click", function () {
       $("#tab-3").prop("checked", true);
-      $(".backGroundPracticeRight").css("display", "block");
+      $("div.interval-timer ").css("display", "block");
       $(".dragGrid").css("display", "none");
       $(".showChoosePractice").css("display", "none");
+      $(".btnListShowStar").css("display", "none");
+      $(".btnListChoose").addClass("active1");
+      $(".PagePractice .content").addClass("active2");
     });
-    $(".labelTotal").on("click", function () {
-      $(".backGroundPracticeRight").css("display", "none");
+    $(".btnListChoose ").on("click", function () {
+      $("div.interval-timer ").css("display", "none");
       $("#tab-2").prop("checked", true);
       $(".showChoosePractice").css("display", "block");
       $(".dragGrid").css("display", "block");
+      $("#tab-choose").prop("checked", true);
+      $(".btnListChoose").addClass("active");
+      $(".btnListChoose").removeClass("active1");
+      $(".btnListShowStar").addClass("active");
+      $(".PagePractice .content").removeClass("active2");
+
+      // $(".btnListShowStar").css("margin", "");
     });
-    $(".listCardPractice").on("click", function () {
-      $(".backGroundPracticeRight").css("display", "none");
+    $(".btnListShowStar").on("click", function () {
+      $("div.interval-timer ").css("display", "none");
       $("#tab-1").prop("checked", true);
       $(".dragGrid").css("display", "block");
       $(".showChoosePractice").css("display", "block");
+      $("#tab-home").prop("checked", true);
+      $(".btnListChoose").removeClass("active");
+
+      $(".btnListShowStar").removeClass("active");
     });
+    // ===========================================
+    // if ($("#tab-choose").is(":checked")) {
+    //   alert("fkj");
+    // }
 
     // ============================================
 
